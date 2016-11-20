f3b7d5770822b7ea794759e935f0e9189da11341
Vladic K <vladic.kostin@secondstreet.com>
2016-11-19 21:22:54 -0600

    Completed step 4.

--- app/routes/album.js
+++ app/routes/album.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import albums from 'bumbox/models/album-fixtures';
 
 export default Ember.Route.extend({
-  model(params) {
-    return albums.findBy('id', params.album_id);
+  model(param) {
+    return albums.findBy('id', param.album_id);
   }
 });
