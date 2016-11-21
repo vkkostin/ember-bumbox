472e129c33c195d06171ef3b0ea44c55073125db
Vladic K <vladic.kostin@secondstreet.com>
2016-11-21 13:28:47 -0600

    Cleaned up helper.

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -3,10 +3,8 @@ import Ember from 'ember';
 export function formatDuration(duration) {
   let minutes = Math.floor(duration / 60);
   let seconds = duration % 60;
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  return minutes + ":" + seconds;
+  let formattedSec = (seconds < 10) ? "0" + seconds : seconds
+  return minutes + ":" + formattedSec;
 }
 
 export default Ember.Helper.helper(formatDuration);
